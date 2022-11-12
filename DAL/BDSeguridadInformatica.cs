3b802d7a84e20cc353ca1e1247fb9bceb630ca47
Marvin Antonio Mejía Espinoza <srgmejia@icloud.com>
2022-11-12 17:01:55 -0600

    Inicializando Repositorio.

---
+++ DAL/BDSeguridadInformatica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using EL;
+
+namespace DAL
+{
+    public class BDSeguridadInformatica : DbContext
+    {
+        public BDSeguridadInformatica()
+       : base(Conexion.ConexionString())
+        {
+        }
+
+        public virtual DbSet<Rol> Rol { get; set; }
+
+    }
+}
---
+++ DAL/DAL_Rol.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL;
+
+namespace DAL
+{
+    public static class DAL_Rol
+    {
+        public static Rol InsertRol(Rol Entidad)
+        {
+            using (BDSeguridadInformatica bd = new BDSeguridadInformatica())
+            {
+                Entidad.FechaRegistro = DateTime.Now;
+                Entidad.Activo = true;
+                bd.Rol.Add(Entidad);
+                bd.SaveChanges();
+                return Entidad;
+            }
+        }
+
+        public static bool UpdateRol(Rol Entidad)
+        {
+            using (BDSeguridadInformatica bd = new BDSeguridadInformatica())
+            {
+                var Consulta = (from tabla in bd.Rol where tabla.Activo && tabla.IdRol == Entidad.IdRol select tabla).Single();
+                Consulta.Descripcion = Entidad.Descripcion;
+                Consulta.IdUsuarioActualiza = Entidad.IdUsuarioActualiza;
+                Consulta.FechaActualizacion = DateTime.Now;
+                return bd.SaveChanges() > 0;
+            }
+        }
+
+        public static List<Rol> ListarRoles(bool Activo=true)
+        {
+            using (BDSeguridadInformatica bd = new BDSeguridadInformatica())
+            {
+                var Consulta = (from tabla in bd.Rol where tabla.Activo == Activo select tabla).ToList();
+                return Consulta;
+            }
+        }
+
+    }
+}
